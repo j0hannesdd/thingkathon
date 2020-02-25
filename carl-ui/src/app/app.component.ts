debc083e6b7b6f877e074d0e564c9bb5218f66f0
Heinrich Haubold <haubold.heinrich@gmail.com>
2020-02-25 15:50:44 +0100

    Adds event trigger

--- carl-ui/src/app/app.component.ts
+++ carl-ui/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { BackendService } from './services/backend.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'carl-ui';
+
+  constructor(public service: BackendService) {
+
+  }
+
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    if(event.keyCode == KEY_CODE.E){
+      console.log(event);
+      this.service.createEvent();
+    }
+  }
+
+}
+
+export enum KEY_CODE {
+    UP_ARROW = 38,
+    DOWN_ARROW = 40,
+    RIGHT_ARROW = 39,
+    LEFT_ARROW = 37,
+    E = 69
 }
