b4e3f585baf9ed49ba906a4c50ae13a9fa854fba
Heinrich Haubold <haubold.heinrich@gmail.com>
2020-02-25 16:56:26 +0100

    Adds event dialog

---
+++ carl-ui/src/app/components/event-dialog/event-dialog.component.ts
@@ -0,0 +1,18 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-event-dialog',
+  templateUrl: './event-dialog.component.html',
+  styleUrls: ['./event-dialog.component.css']
+})
+export class EventDialogComponent implements OnInit {
+
+  constructor(
+      public dialogRef: MatDialogRef<EventDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+  ngOnInit() {
+  }
+
+}
