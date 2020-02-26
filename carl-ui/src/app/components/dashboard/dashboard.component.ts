537ae845007bd7daa5ced08b470ab6d03babd41b
Heinrich Haubold <haubold.heinrich@gmail.com>
2020-02-26 12:21:57 +0100

    Heat map animation

--- carl-ui/src/app/components/dashboard/dashboard.component.ts
+++ carl-ui/src/app/components/dashboard/dashboard.component.ts
@@ -17,9 +17,26 @@ export class DashboardComponent implements OnInit {
         [1.0,1.0,1.0,1.0,1.0, 1.0,1.0,1.0,1.0,1.0]
     ];
 
+    interval: any;
+
   constructor() { }
 
   ngOnInit() {
+      this.interval = setInterval(() => {
+          const value = Math.random();
+          if(value < 0.33) return;
+          const row = this.getRndInteger(0, 6);
+          const col = this.getRndInteger(0, 10);
+          if(this.heatData[row][col] >= 0.33) this.heatData[row][col] = value;
+      }, 1000);
+  }
+
+  getRndInteger(min, max) {
+      return Math.floor(Math.random() * (max - min + 1) ) + min;
+    }
+
+  ngOnDestroy() {
+      clearInterval(this.invteral);
   }
 
 }
